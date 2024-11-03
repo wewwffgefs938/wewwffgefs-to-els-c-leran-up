9fc8bd2fecf1a5bbe7aa81c205a3c34fa41e9283
Els_Rc <els_rction@zohomail.com>
2024-11-03 22:23:11 +0800

    the third dimension express

--- two demensional arr.c
+++ two demensional arr.c
@@ -1,8 +1,8 @@
-// one demensional array is continuos memory location --example 0x0 0x4 0x8(the data of int)
+// one dimensional array is continuos memory location --example 0x0 0x4 0x8(the data of int)
 
 //pay attention :  null in origin is 0(int) in int
 
-// two demensional array is other continuos memory location in other direction? no,the direction is straight
+// two dimensional array is other continuos memory location in other direction? no,the direction is straight
 
 //Example  the direction of arr[2][10] : 2 to y , 10 to x
 
@@ -12,7 +12,7 @@
 
 int main(){
     /*can int arr[][];
-        to make a demensional array*/
+        to make a dimensional array*/
 // need to make origin
     int arr0[2][10] ={{1,2,3,4,5,6,7,8,9,10},
                     {10,9,8,7,6,5,4,3,2,1}
@@ -20,13 +20,13 @@ int main(){
     int arr1[2][10] ={1,2,3,4,5,6,7,8,9,10,10,9,8,7,6,5,4,3,2,1};
 
 // So i can get a conclusion that is read x priorly and then to read the x of the next y;
-// the way of save is same as one demensional array;
+// the way of save is same as one dimensional array;
 /*example:
     arr[0][0] 1x0
     arr[1][0] 1x0 + (size of int == 4)* 10 == 1x40
     */
 // the mistakes of  same original snese
-// exmaple 
+// example
     /*
     int arr[];
     int arr[2][]
@@ -62,6 +62,38 @@ int main(){
         printf("\n");
         
     }
+    //the same sense of origin
+    //所谓几维数值，就是那大括号代表代表维度的方向
+    // so the third dimension
+printf("the third example is:\n");
+    int arr3[2][3][4] = {
+        {
+            {1,2,3,4},
+            {4,3,2,1},
+            {8,8,8,8}
+        },
+        {
+            {8,8,8,8},
+            {6,6,6,6},
+            {1,3,4,5}
+
+        }
+    };
+    for (int i = 0; i < 2; i++)
+    {
+        for (int k = 0; k < 3; k++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                printf("%d",arr3[i][k][j]);
+            }
+            printf("\n");
+            
+        }
+        printf("\n");
+        
+    }
+    
     
     return 0 ;
     
