c15f16672d25db5ce51295637510ce27af5e948c
Els_Rc <els_rction@zohomail.com>
2024-11-04 23:36:15 +0800

    push box

---
+++ the game of push box.c
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+int main() {
+
+    int arrbox[5][5] = { // a arr express area ,the 2 express the box
+        {1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 1},
+        {1, 0, 2, 3, 1},
+        {1, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1}
+    };
+
+    int x = 0, y = 0;      // to represent the coordinate of player
+    int xbox = 0, ybox = 0; // to represent the coordinate of box
+
+    while (1) {
+        for (int i = 0; i < 5; i++) {
+            for (int k = 0; k < 5; k++) {
+                if (arrbox[i][k] == 2) { // get the data of coordinate of player
+                    x = k;
+                    y = i;
+                }
+                if (arrbox[i][k] == 3) { // get the data of coordinate of box
+                    xbox = k;
+                    ybox = i;
+                }
+            }
+        }
+
+        system("cls"); // clear the screen
+
+        for (int i = 0; i < 5; i++) { // print the area image
+            for (int k = 0; k < 5; k++) {
+                printf("%d", arrbox[i][k]);
+            }
+            printf("\n");
+        }
+
+        switch (_getch()) { // get the key of keyboard
+        case 's':
+            if (arrbox[y + 1][x] == 1) {
+                break;
+            }
+            if (arrbox[y + 1][x] == 3) {
+                if (arrbox[ybox + 1][xbox] == 1) {
+                    arrbox[y][x] += 1;
+                    arrbox[y + 1][x] -= 1;
+                    break;
+                }
+                arrbox[ybox + 1][xbox] += 3;
+                arrbox[ybox][xbox] -= 1;
+                arrbox[y][x] -= 2;
+                break;
+            }
+            arrbox[y][x] -= 2;
+            arrbox[y + 1][x] += 2; // exchange coordinate of player
+            break;
+
+        case 'w':
+            if (arrbox[y - 1][x] == 1) {
+                break;
+            }
+            if (arrbox[y - 1][x] == 3) {
+                if (arrbox[ybox - 1][xbox] == 1) {
+                    arrbox[y][x] += 1;
+                    arrbox[y - 1][x] -= 1;
+                    break;
+                }
+                arrbox[ybox - 1][xbox] += 3;
+                arrbox[ybox][xbox] -= 1;
+                arrbox[y][x] -= 2;
+                break;
+            }
+            arrbox[y][x] -= 2;
+            arrbox[y - 1][x] += 2;
+            break;
+
+        case 'a':
+            if (arrbox[y][x - 1] == 1) {
+                break;
+            }
+            if (arrbox[y][x - 1] == 3) {
+                if (arrbox[ybox][xbox - 1] == 1) {
+                    arrbox[y][x] += 1;
+                    arrbox[y][x - 1] -= 1;
+                    break;
+                }
+                arrbox[ybox][xbox - 1] += 3;
+                arrbox[ybox][xbox] -= 1;
+                arrbox[y][x] -= 2;
+                break;
+            }
+            arrbox[y][x] -= 2;
+            arrbox[y][x - 1] += 2;
+            break;
+
+        case 'd':
+            if (arrbox[y][x + 1] == 1) {
+                break;
+            }
+            if (arrbox[y][x + 1] == 3) {
+                if (arrbox[ybox][xbox + 1] == 1) {
+                    arrbox[y][x] += 1;
+                    arrbox[y][x + 1] -= 1;
+                    break;
+                }
+                arrbox[ybox][xbox + 1] += 3;
+                arrbox[ybox][xbox] -= 1;
+                arrbox[y][x] -= 2;
+                break;
+            }
+            arrbox[y][x] -= 2;
+            arrbox[y][x + 1] += 2;
+            break;
+
+        default:
+            break;
+        }
+    }
+    return 0;
+}
